0eb0e06311c7fea1de3907706964467d252fd902
Eldar <97941804+ElderNE@users.noreply.github.com>
2024-05-27 15:06:37 -0400

    Add one
    Start with 1.0.0 v

---
+++ index.d.tsx
@@ -0,0 +1,69 @@
+"use strict";
+
+import React from "react";
+
+type PropsIn = {
+    scriptIn:   string,
+    divIn:      string,
+    widthIn:    string,
+    heightIn:   string,
+    zIndex:     string,
+    leftIn:     string,
+    topIn:      string,
+    rightIn:    string,
+    bottomIn:   string,
+    title:      string,
+    position:   "static" | "relative" | "absolute" | "sticky" | "fixed",
+    background: string,
+}
+
+export const WidgetContainer: React.FC<PropsIn> = (props:PropsIn) => {
+
+    const { 
+        scriptIn    = "<script></script>",
+        divIn       = "<div></div>",
+        widthIn     = "0",
+        heightIn    = "0",
+        zIndex      = "0",
+        leftIn      = "auto",
+        topIn       = "auto",
+        rightIn     = "auto",
+        bottomIn    = "auto",
+        title       = "Clatch Widget",
+        position    = "fixed",
+        background  = "rgba(255,255,255,0.8)"
+    } = props;
+
+    const style: React.CSSProperties = {
+            position:   position,
+            bottom:     bottomIn,
+            left:       leftIn,
+            top:        topIn,
+            right:      rightIn,
+            zIndex:     zIndex,
+            width:      widthIn,
+            height:     heightIn,
+            overflow:   "hidden",
+            background: background,
+    }
+
+    const srcSource = 
+    `<!DOCTYPE html>
+        <head>
+            ${scriptIn}
+        </head>
+        <body>
+            ${divIn}
+        </body>
+    </html>`;
+
+    return (
+        <div style={style}>
+            <iframe srcDoc={srcSource}
+                    title={title}
+                    style={{width: widthIn, height: heightIn, border: "none"}}>
+            </iframe>
+        </div>
+    );
+  }
+  
---
+++ index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+export function WidgetContainer(props) {
+
+    const { 
+        scriptIn    = "<script></script>",
+        divIn       = "<div></div>",
+        widthIn     = "0",
+        heightIn    = "0",
+        zIndex      = "0",
+        leftIn      = "auto",
+        topIn       = "auto",
+        rightIn     = "auto",
+        bottomIn    = "auto",
+        title       = "Clatch Widget",
+        position    = "fixed",
+        background  = "rgba(255,255,255,0.8)"
+    } = props;
+
+    const style = {
+            position:   position,
+            bottom:     bottomIn,
+            left:       leftIn,
+            top:        topIn,
+            right:      rightIn,
+            zIndex:     zIndex,
+            width:      widthIn,
+            height:     heightIn,
+            overflow:   "hidden",
+            background: background,
+    }
+
+    const srcSource = 
+    `<!DOCTYPE html>
+        <head>
+            ${scriptIn}
+        </head>
+        <body>
+            ${divIn}
+        </body>
+    </html>`;
+
+    return (
+        <div style={style}>
+            <iframe srcDoc={srcSource}
+                    title={title}
+                    style={{width: widthIn, height: heightIn, border: "none"}}>
+            </iframe>
+        </div>
+    );
+  }
